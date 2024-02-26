d74c47ea93866c0b1049cb3d3a6c981db7e86dde
jorgeodominguez <jorgeodominguez@hotmail.com>
2024-02-26 09:34:42 -0600

    2do video

--- app/hola_mundo/mensaje/models.py
+++ app/hola_mundo/mensaje/models.py
@@ -15,7 +15,7 @@ COLOR=[
 
 class Articulos(models.Model):
     nombre=models.CharField(max_length=100)
-    descripcion=models.TextField('Descripción')
+    descripcion=models.TextField('Descripción',null=True,blank=True)
     stock=models.IntegerField()
     presentacion=models.CharField('Presentación',max_length=1,choices=PRESENTACION)
     color=models.CharField(max_length=1,choices=COLOR)
