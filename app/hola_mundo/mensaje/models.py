d59bf94ec4597dd948a3574c5b0c9c0220f1eb20
jorgeodominguez <jorgeodominguez@hotmail.com>
2024-02-25 19:02:03 -0600

    video 2 1 hora

--- app/hola_mundo/mensaje/models.py
+++ app/hola_mundo/mensaje/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
+#CHOISES
+PRESENTACION=[
+    ('1','Caja'),
+    ('2','Pieza'),
+    ('3','Paquete')
+]
+COLOR=[
+    ('r','Rojo'),
+    ('a','Azul'),
+    ('v','Verde')
+]
+
+
+class Articulos(models.Model):
+    nombre=models.CharField(max_length=100)
+    descripcion=models.TextField('Descripción')
+    stock=models.IntegerField()
+    presentacion=models.CharField('Presentación',max_length=1,choices=PRESENTACION)
+    color=models.CharField(max_length=1,choices=COLOR)
+
+
+
 # Create your models here.
